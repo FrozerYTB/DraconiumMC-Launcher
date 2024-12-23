38bf58d25e9942ef6d6a5a32ef449a07ffb2360e
FrozerYTB <frozerytb3@gmail.com>
2024-12-23 13:08:10 +0100

    test
    test

--- app/assets/js/dropinmodutil.js
+++ app/assets/js/dropinmodutil.js
@@ -1,11 +1,6 @@
-const fs        = require('fs-extra')
-const path      = require('path')
-const { ipcRenderer, shell } = require('electron')
-const { SHELL_OPCODE } = require('./ipcconstants')
+const fs = require('fs-extra')
+const path = require('path')
 
-// Group #1: File Name (without .disabled, if any)
-// Group #2: File Extension (jar, zip, or litemod)
-// Group #3: If it is disabled (if string 'disabled' is present)
 const MOD_REGEX = /^(.+(jar|zip|litemod))(?:\.(disabled))?$/
 const DISABLED_EXT = '.disabled'
 
@@ -14,10 +9,13 @@ const SHADER_OPTION = /shaderPack=(.+)/
 const SHADER_DIR = 'shaderpacks'
 const SHADER_CONFIG = 'optionsshaders.txt'
 
+// Liste blanche des mods autorisés
+const allowedMods = ['mod1.jar', 'mod2.jar', 'mod3.jar']
+
 /**
  * Validate that the given directory exists. If not, it is
  * created.
- * 
+ *
  * @param {string} modsDir The path to the mods directory.
  */
 exports.validateDir = function(dir) {
@@ -27,10 +25,10 @@ exports.validateDir = function(dir) {
 /**
  * Scan for drop-in mods in both the mods folder and version
  * safe mods folder.
- * 
+ *
  * @param {string} modsDir The path to the mods directory.
  * @param {string} version The minecraft version of the server configuration.
- * 
+ *
  * @returns {{fullName: string, name: string, ext: string, disabled: boolean}[]}
  * An array of objects storing metadata about each discovered mod.
  */
@@ -46,23 +44,33 @@ exports.scanForDropinMods = function(modsDir, version) {
         for(let file of modCandidates){
             const match = MOD_REGEX.exec(file)
             if(match != null){
-                modsDiscovered.push({
-                    fullName: match[0],
-                    name: match[1],
-                    ext: match[2],
-                    disabled: match[3] != null
-                })
+                if (allowedMods.includes(match[0])) {
+                    modsDiscovered.push({
+                        fullName: match[0],
+                        name: match[1],
+                        ext: match[2],
+                        disabled: match[3] != null
+                    })
+                } else {
+                    console.warn(`Mod non autorisé supprimé : ${file}`)
+                    fs.unlinkSync(path.join(modsDir, file)) // Supprime le mod non autorisé
+                }
             }
         }
         for(let file of verCandidates){
             const match = MOD_REGEX.exec(file)
             if(match != null){
-                modsDiscovered.push({
-                    fullName: path.join(version, match[0]),
-                    name: match[1],
-                    ext: match[2],
-                    disabled: match[3] != null
-                })
+                if (allowedMods.includes(match[0])) {
+                    modsDiscovered.push({
+                        fullName: path.join(version, match[0]),
+                        name: match[1],
+                        ext: match[2],
+                        disabled: match[3] != null
+                    })
+                } else {
+                    console.warn(`Mod non autorisé supprimé : ${file}`)
+                    fs.unlinkSync(path.join(versionDir, file)) // Supprime le mod non autorisé
+                }
             }
         }
     }
@@ -70,73 +78,46 @@ exports.scanForDropinMods = function(modsDir, version) {
 }
 
 /**
- * Add dropin mods.
- * 
- * @param {FileList} files The files to add.
- * @param {string} modsDir The path to the mods directory.
+ * Add drop-in mods.
+ * Désactivé pour empêcher l'ajout de mods.
  */
-exports.addDropinMods = function(files, modsdir) {
-
-    exports.validateDir(modsdir)
-
-    for(let f of files) {
-        if(MOD_REGEX.exec(f.name) != null) {
-            fs.moveSync(f.path, path.join(modsdir, f.name))
-        }
-    }
-
+exports.addDropinMods = function(files, modsDir) {
+    console.warn('Ajout de mods désactivé.')
+    return // Empêche l'ajout de mods
 }
 
 /**
  * Delete a drop-in mod from the file system.
- * 
+ * Désactivé pour empêcher la suppression de mods.
+ *
  * @param {string} modsDir The path to the mods directory.
  * @param {string} fullName The fullName of the discovered mod to delete.
- * 
- * @returns {Promise.<boolean>} True if the mod was deleted, otherwise false.
+ *
+ * @returns {Promise.<boolean>} Always false, car désactivé.
  */
 exports.deleteDropinMod = async function(modsDir, fullName){
-
-    const res = await ipcRenderer.invoke(SHELL_OPCODE.TRASH_ITEM, path.join(modsDir, fullName))
-
-    if(!res.result) {
-        shell.beep()
-        console.error('Error deleting drop-in mod.', res.error)
-        return false
-    }
-
-    return true
+    console.warn('Suppression de mods désactivée.')
+    return false // Indique que l'action a échoué
 }
 
 /**
- * Toggle a discovered mod on or off. This is achieved by either 
- * adding or disabling the .disabled extension to the local file.
- * 
+ * Toggle a discovered mod on or off.
+ * Désactivé pour empêcher les modifications d'état des mods.
+ *
  * @param {string} modsDir The path to the mods directory.
  * @param {string} fullName The fullName of the discovered mod to toggle.
  * @param {boolean} enable Whether to toggle on or off the mod.
- * 
- * @returns {Promise.<void>} A promise which resolves when the mod has
- * been toggled. If an IO error occurs the promise will be rejected.
+ *
+ * @returns {Promise.<void>} A promise which resolves immédiatement.
  */
 exports.toggleDropinMod = function(modsDir, fullName, enable){
-    return new Promise((resolve, reject) => {
-        const oldPath = path.join(modsDir, fullName)
-        const newPath = path.join(modsDir, enable ? fullName.substring(0, fullName.indexOf(DISABLED_EXT)) : fullName + DISABLED_EXT)
-
-        fs.rename(oldPath, newPath, (err) => {
-            if(err){
-                reject(err)
-            } else {
-                resolve()
-            }
-        })
-    })
+    console.warn('Basculement des mods désactivé.')
+    return Promise.resolve() // Empêche le basculement sans provoquer d'erreur
 }
 
 /**
  * Check if a drop-in mod is enabled.
- * 
+ *
  * @param {string} fullName The fullName of the discovered mod to toggle.
  * @returns {boolean} True if the mod is enabled, otherwise false.
  */
@@ -144,11 +125,14 @@ exports.isDropinModEnabled = function(fullName){
     return !fullName.endsWith(DISABLED_EXT)
 }
 
+/* Les fonctions liées aux shaderpacks restent inchangées */
+
+
 /**
  * Scan for shaderpacks inside the shaderpacks folder.
- * 
+ *
  * @param {string} instanceDir The path to the server instance directory.
- * 
+ *
  * @returns {{fullName: string, name: string}[]}
  * An array of objects storing metadata about each discovered shaderpack.
  */
@@ -176,9 +160,9 @@ exports.scanForShaderpacks = function(instanceDir){
 /**
  * Read the optionsshaders.txt file to locate the current
  * enabled pack. If the file does not exist, OFF is returned.
- * 
+ *
  * @param {string} instanceDir The path to the server instance directory.
- * 
+ *
  * @returns {string} The file name of the enabled shaderpack.
  */
 exports.getEnabledShaderpack = function(instanceDir){
@@ -199,7 +183,7 @@ exports.getEnabledShaderpack = function(instanceDir){
 
 /**
  * Set the enabled shaderpack.
- * 
+ *
  * @param {string} instanceDir The path to the server instance directory.
  * @param {string} pack the file name of the shaderpack.
  */
@@ -219,7 +203,7 @@ exports.setEnabledShaderpack = function(instanceDir, pack){
 
 /**
  * Add shaderpacks.
- * 
+ *
  * @param {FileList} files The files to add.
  * @param {string} instanceDir The path to the server instance directory.
  */
